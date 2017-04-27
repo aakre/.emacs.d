4708f947af7225667514c5e811aaac0687fb6b7d
Øyvind <oyvind.aakre@gmail.com>
2017-04-27 13:13:16 +0200

    Added comments to setup-windows.el

--- custom/setup-windows.el
+++ custom/setup-windows.el
@@ -7,12 +7,12 @@
 
 ;; Changes to the window to the right
 (global-unset-key (kbd "<M-right>"))
-(global-set-key (kbd "<M-right>") (lambda () (interactive) (other-window 1)))
+(global-set-key (kbd "<M-right>") (lambda () "Changes focus to the next window" (interactive) (other-window 1)))
 
 
 ;; Changes to the window to the left
 (global-unset-key (kbd "<M-left>"))
-(global-set-key (kbd "<M-left>") (lambda () (interactive) (other-window -1)))
+(global-set-key (kbd "<M-left>") (lambda () "Changes focus to the previous window" (interactive) (other-window -1)))
 
 (provide 'setup-windows)
 ;;; setup-windows.el ends here
